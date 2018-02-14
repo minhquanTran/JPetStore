30488ef7dfdbae518dc57d2aa8ce3165a108e0f9
quancachan <mqtran.cachan@gmail.com>
2018-02-14 10:53:52 +0100

    First commit

---
+++ src/test/java/fr/henix/autom/PetStoreTest.java
@@ -0,0 +1,44 @@
+package fr.henix.autom;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class PetStoreTest {
+
+	WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		System.out.println("here : " + System.getProperty("browser"));
+		
+		FirefoxOptions opts = new FirefoxOptions().setProfile(new FirefoxProfile());
+		opts.addPreference("browser.tabs.remote.autostart", false);
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\new_data_QUAN\\HENIX\\SQUASH_TA_FORMATION\\Formation\\Selenium Drivers\\geckodriver.exe");
+		driver = new FirefoxDriver(opts);
+		driver.get("http://localhost:8180/jpetstore");
+	}
+	
+	@Test
+	public void test1() {
+//		System.setProperty("webdriver.chrome.driver", "D:\\new_data_QUAN\\HENIX\\SQUASH_TA_FORMATION\\Formation\\Selenium Drivers\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+		
+		WebElement myButton = driver.findElement(By.linkText("Enter the Store"));
+		myButton.click();
+		
+//		WebElement myElementToBeChecked = driver.findElement(By.linkText("Sign In"));
+		
+		WebElement myElementToBeChecked = driver.findElement(By.xpath(".//*[@id='SidebarContent']/a[2]/img"));
+		System.out.println(myElementToBeChecked.getAttribute("src"));
+		Assert.assertEquals("http://localhost:8180/jpetstore/images/dogs_icon.gif", myElementToBeChecked.getAttribute("src"));
+	}
+}
